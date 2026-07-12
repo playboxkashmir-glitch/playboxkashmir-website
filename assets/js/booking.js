46eef1dadde655f0abd2024e6940d12b196f9212
playboxkashmir-glitch <playboxkashmir@gmail.com>
2026-07-12 13:07:00 +0530

    Add trademark symbol to Razorpay checkout name field

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,4 +1,4 @@
-const SPORT_META = {
+™const SPORT_META = {
    football: { name: 'Football Turf', icon: 'fas fa-futbol', color: '#15803d', comingSoon: false }, boxcricket: { name: 'Box Cricket', icon: 'fas fa-baseball-ball', color: '#ea580c', comingSoon: false },
    cricket: { name: 'Cricket Nets', icon: 'fas fa-baseball-ball', color: '#22c55e', comingSoon: true },
    pickleball: { name: 'Pickleball Court', icon: 'fas fa-table-tennis', color: '#22c55e', comingSoon: true }
@@ -566,7 +566,7 @@ function openRazorpay() {
       key: 'rzp_live_T90dB0bfW4qEMO',
       amount: Math.round(state.totalAmount * 100),
       currency: 'INR',
-      name: 'PlayBox Kashmir',
+      name: 'PlayBox Kashmir™',
       description: state.facilityName + ' - ' + state.slotLabel + ' on ' + state.dateFormatted,
       image: 'https://playboxkashmir.com/assets/images/logo.png',
       order_id: state.razorpayOrderId,
